8a03ba3de0969e217c80f6d6bd6c608b522f77cc
Matheus <ra23371012-2@aluno.unicesumar.edu.br>
2025-05-30 18:06:16 -0300

    ajuste no commands para senha

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,7 +2,7 @@ Cypress.Commands.add('login', () => {
   cy.fixture('usuario.json').then((user) => {
   cy.visit('https://buggy.justtestit.org/');
   cy.get('input[placeholder="Login"]').type(user.usuario);
-  cy.get('input[name="password"][type="password"]').type(user.senha);
+  cy.get('input[name="password"][type="password"]').type('Test@1234');
   cy.get('.btn-success').click();
   });
 });

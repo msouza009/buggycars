7f02f3f7a966f45fb2ea61fe281d6568c442792e
msouza009 <160975825+msouza009@users.noreply.github.com>
2025-05-30 17:26:15 -0300

    Revert "automação para o profile"

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,25 @@
-Cypress.Commands.add('login', () => {
-  cy.visit('https://buggy.justtestit.org/');
-  cy.get('input[placeholder="Login"]').type('Davi_Macedo');
-  cy.get('input[name="password"][type="password"]').type('Test@1234');
-  cy.get('.btn-success').click();
-  cy.contains('Hi, Davi').should('be.visible');
-});
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
